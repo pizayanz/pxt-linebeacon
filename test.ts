9ec855fa24611669a0061591ec64c26c10a3e08b
Tomoyuki Hisasda <tom@anotherbrain.co.jp>
2018-04-03 09:08:17 +0900

    add test

--- test.ts
+++ test.ts
@@ -1,1 +1,10 @@
-// tests need line beacon HWID and some smartphones which are installed line app and settings.
+linebeacon.start("0f0f0f0f0f"); // this is fake HWID
+
+linebeacon.stop();
+
+linebeacon.startWithDeviceMessage(
+    "0f0f0f0f0f",   // this is also fake HWID
+    "1a2b3c4d5e6f70809010a0b0c0"
+);
+
+linebeacon.stop();
